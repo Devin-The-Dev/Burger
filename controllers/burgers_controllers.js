08703263c2fc00a3610ce28c9c1ae9d982c41cf3
Devin Udy <devin.udy@gmail.com>
2019-02-20 23:40:08 -0700

    Pushing to heroku

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,5 +1,37 @@
 var express = require('express');
-//import burger.js
 
+var router = express.Router();
+//import burger.js
+var burger = require("../models/burger.js");
 
 //Create the `router` for the app, and export the `router` at the end of your file.
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", function (req, res) {
+    burgers.insertOne(["burger_name"], [req.body.name], function (result) {
+        // Send back the ID of the new quote
+        res.json({
+            id: result.insertId
+        });
+    });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+    burgers.updateOne(["devoured"], [req.body.devour], [req.params.id], function (result) {
+        if (result.changedRows === 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        }
+        res.status(200).end();
+    });
+});
+
+module.exports = router;
