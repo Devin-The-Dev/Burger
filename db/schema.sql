5d13ca501b11a7ff88d2ddfb36fc1111c8d04372
Devin Udy <devin.udy@gmail.com>
2019-02-21 16:27:09 -0700

    Got schema.sql to work, deleted seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,13 @@ USE burgers_db;
 
 CREATE TABLE burgers (
     PRIMARY KEY (id),
-    id INT NOT NULL,
-    burger_name VARCHAR(20) NULL,
-    devoured BOOLEAN DEFAULT false
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(50) NULL,
+    devoured BOOLEAN
 );
+
+INSERT INTO burgers (burger_name) VALUES ('Peas and Thank You Burger');
+INSERT INTO burgers (burger_name) VALUES ('National Pass-Thyme Burger');
+INSERT INTO burgers (burger_name) VALUES ('Gourdon-Hamsey Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cheeses Is Born Burger');
+INSERT INTO burgers (burger_name) VALUES ('Open Sesame Burger');
--- db/seeds.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO burgers (burger_name, devoured) VALUES (
-    (`Peas and Thank You Burger`, false),
-    (`National Pass-Thyme Burger`, false),
-    (`Gourdon-Hamsey Burger`, false),
-    (`Cheeses Is Born Burger`, false),
-    (`Open Sesame Burger`, false)
-);
-
-FROM burgers_db.burgers;
